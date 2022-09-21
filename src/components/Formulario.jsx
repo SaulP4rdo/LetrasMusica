24cceda9bbb062c3e65c4717eb5207a9da16e824
Saul Pardo <sul_p4rdo@icloud.com>
2022-09-21 03:00:00 -0500

    Añadiendo el formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,5 +1,48 @@
+import { useState } from "react";
 const Formulario = () => {
-  return <div>Formulario</div>;
+  const [busqueda, setBusqueda] = useState({
+    artista: "",
+    cancion: "",
+  });
+
+  return (
+    <form action="">
+      <legend>BUscar por artista y cancion</legend>
+      <div className="form-grid">
+        <div className="">
+          <label htmlFor="">Artista</label>
+          <input
+            type="text"
+            name="artista"
+            placeholder="Nombre Artista"
+            value={busqueda.artista}
+            onChange={(e) =>
+              setBusqueda({
+                ...busqueda,
+                [e.target.name]: e.target.value,
+              })
+            }
+          />
+        </div>
+        <div className="">
+          <label htmlFor="">Cancion</label>
+          <input
+            type="text"
+            name="cancion"
+            placeholder="Nombre Cancion"
+            value={busqueda.cancion}
+            onChange={(e) =>
+              setBusqueda({
+                ...busqueda,
+                [e.target.name]: e.target.value,
+              })
+            }
+          />
+        </div>
+        <input type="submit" value={"Buscar"} />
+      </div>
+    </form>
+  );
 };
 
 export default Formulario;
